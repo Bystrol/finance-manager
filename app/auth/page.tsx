cbca50f022262ce7f043e57a04751c7f65a8506f
Michal <michalbystryk14@wp.pl>
2023-05-08 16:05:51 +0200

    replaced input with input component

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,39 +1,31 @@
+import Input from "@/components/Input";
+import { useState } from "react";
+
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="flex justify-center items-center w-full h-screen">
       <div className="flex bg-white w-[50%] h-[60vh] rounded-xl p-4">
-        <div className="flex flex-col items-center justify-center w-[50%] h-full text-center">
+        <div className="flex flex-col items-center justify-center w-[50%] h-full text-center gap-3">
           <h1 className="font-bold text-2xl">Welcome back!</h1>
-          <h2 className=" text-sm mt-2 text-zinc-600">
+          <h2 className=" text-sm text-zinc-600 mb-5">
             Please enter your details.
           </h2>
-          <input
-            className="
-            w-[70%]
-            h-[4vh]
-            border-2
-            border-solid
-            rounded-lg
-            text-sm
-            p-4
-            mt-8
-            "
+          <Input
+            id="email"
             type="email"
-            placeholder="Enter your email"
+            onChange={(e: any) => setEmail(e.target.value)}
+            value={email}
+            label="Enter your email"
           />
-          <input
-            className="
-            w-[70%]
-            h-[4vh]
-            border-2
-            border-solid
-            rounded-lg
-            text-sm
-            p-4
-            mt-3
-            "
+          <Input
+            id="password"
             type="password"
-            placeholder="Enter your password"
+            onChange={(e: any) => setPassword(e.target.value)}
+            value={password}
+            label="Enter your password"
           />
         </div>
         <div className="w-[50%] h-full bg-[url('/images/auth-image.jpg')] bg-no-repeat bg-center bg-cover rounded-lg"></div>
