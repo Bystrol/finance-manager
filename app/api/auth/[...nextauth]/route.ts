6426eced60386bd1f062e2a583a70fa4c3019592
Michal <michalbystryk14@wp.pl>
2023-05-10 15:35:31 +0200

    added google oauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/lib/prisma";
 import Credentials from "next-auth/providers/credentials";
 import { compare } from "bcrypt";
@@ -10,6 +11,10 @@ export const authOptions: AuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
     }),
     Credentials({
       id: "credentials",
