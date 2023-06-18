1bca20eeeff81efad41cb0aefd8104829cb9ddeb
Michał <michalbystryk14@wp.pl>
2023-06-18 19:17:07 +0200

    add username, email and password validation

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -3,11 +3,36 @@ import { hash } from "bcrypt";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  const validateEmail = (email: string) => {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+  };
+
+  const validatePassword = (password: string) => {
+    return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password);
+  };
+
   try {
     const { email, username, password } = await req.json();
 
     if (!email || !username || !password) {
       return new NextResponse("Missing fields", { status: 400 });
+    }
+
+    if (username.length < 3) {
+      return new NextResponse("Username must consist of minimum 3 characters", {
+        status: 422,
+      });
+    }
+
+    if (!validateEmail(email)) {
+      return new NextResponse("Invalid email", { status: 422 });
+    }
+
+    if (!validatePassword(password)) {
+      return new NextResponse(
+        "Password must consist of minimum 8 characters, at least one uppercase letter, one lowercase letter and one number",
+        { status: 422 }
+      );
     }
 
     const userExists = await prisma.user.findUnique({
