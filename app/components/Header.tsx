ca6e9caaa19c854dc3a8c817885dbbf2bca47875
Michal <michalbystryk14@wp.pl>
2023-06-21 20:00:40 +0200

    move profile image to separate component, add menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,61 +1,47 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
-import ProfileImage from "../../public/images/blank-profile-picture.png";
+import ProfileImage from "./ProfileImage";
+import ProfileMenu from "./ProfileMenu";
 import { useSession } from "next-auth/react";
 import { RiMoneyDollarCircleFill } from "react-icons/ri";
 import { HiBars3BottomLeft } from "react-icons/hi2";
 
-const Header = () => {
+const Header: React.FC = () => {
   const { data: session, status } = useSession();
 
-  const imageStyles = {
-    borderRadius: "50%",
-    border: "1px solid #a2a3a5",
-    marginRight: "0.75rem",
-  };
+  const [showProfileMenu, setShowProfileMenu] = useState(false);
+
+  const toggleProfileMenu = useCallback(() => {
+    setShowProfileMenu((state) => !state);
+  }, []);
 
   if (session && status === "authenticated") {
     return (
       <header className="fixed flex justify-between items-center w-full h-12 bg-white border-b-2 border-black">
-        <div className="flex justify-between items-center h-full w-1/2">
-          <HiBars3BottomLeft size={35} className="ml-3" />
+        <div className="flex justify-start items-center h-full w-1/2 gap-x-3">
+          <HiBars3BottomLeft size={35} className="ml-3 cursor-pointer" />
           <Link href="/" className="flex items-center">
             <RiMoneyDollarCircleFill size={35} />
             <h1 className="text-xl font-bold ml-1">FINEances</h1>
           </Link>
         </div>
-        <div className="relative h-8 w-8 top-0 right-3">
-          <Image
-            src={session?.user?.image ? session.user.image : ProfileImage}
-            alt="profile_picture"
-            fill={true}
-            style={imageStyles}
-          />
+        <div
+          className="relative h-8 w-8 top-0 right-3 cursor-pointer"
+          onClick={toggleProfileMenu}
+        >
+          <ProfileImage />
         </div>
+        {showProfileMenu && (
+          <div
+            className="fixed w-full h-[200vh] bg-black/10"
+            onClick={toggleProfileMenu}
+          ></div>
+        )}
+        <ProfileMenu isVisible={showProfileMenu} onClose={toggleProfileMenu} />
       </header>
     );
   }
-
-  return (
-    <header className="fixed flex justify-between items-center w-full h-12 bg-white border-b-2 border-black">
-      <div className="flex justify-between items-center h-full w-1/2">
-        <HiBars3BottomLeft size={35} className="ml-3" />
-        <Link href="/" className="flex items-center">
-          <RiMoneyDollarCircleFill size={35} />
-          <h1 className="text-xl font-bold ml-1">FINEances</h1>
-        </Link>
-      </div>
-      <div className="relative h-8 w-8 top-0 right-3">
-        <Image
-          src={ProfileImage}
-          alt="profile_picture"
-          fill={true}
-          style={imageStyles}
-        />
-      </div>
-    </header>
-  );
+  return <div></div>;
 };
 
 export default Header;
