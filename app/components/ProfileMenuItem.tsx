9f27b29f85f388dadb2b0232920b5d8f638d4bfd
Michal <michalbystryk14@wp.pl>
2023-06-21 20:01:50 +0200

    create ProfileMenuItem component

---
+++ app/components/ProfileMenuItem.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Link from "next/link";
+import { IconType } from "react-icons/lib";
+
+interface ItemProps {
+  href: string;
+  icon: IconType;
+  text: string;
+  onClick?: () => void;
+}
+
+const ProfileMenuItem: React.FC<ItemProps> = ({
+  href,
+  icon,
+  text,
+  onClick,
+}) => {
+  const Icon = icon;
+
+  return (
+    <Link href={href} replace>
+      <li
+        className="flex items-center gap-x-2 cursor-pointer"
+        onClick={onClick}
+      >
+        <Icon size={20} />
+        {text}
+      </li>
+    </Link>
+  );
+};
+
+export default ProfileMenuItem;
