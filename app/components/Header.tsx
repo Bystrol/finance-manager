8f539c8f831f367191b9c02d61d4debb9ca1acba
Michal <michalbystryk14@wp.pl>
2023-06-22 19:51:03 +0200

    remove Bars, add styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,9 @@
 import React, { useCallback, useState } from "react";
 import Link from "next/link";
 import ProfileImage from "./ProfileImage";
-import ProfileMenu from "./ProfileMenu";
+import Menu from "./Menu";
 import { useSession } from "next-auth/react";
 import { RiMoneyDollarCircleFill } from "react-icons/ri";
-import { HiBars3BottomLeft } from "react-icons/hi2";
 
 const Header: React.FC = () => {
   const { data: session, status } = useSession();
@@ -17,28 +16,24 @@ const Header: React.FC = () => {
 
   if (session && status === "authenticated") {
     return (
-      <header className="fixed flex justify-between items-center w-full h-12 bg-white border-b-2 border-black">
-        <div className="flex justify-start items-center h-full w-1/2 gap-x-3">
-          <HiBars3BottomLeft size={35} className="ml-3 cursor-pointer" />
-          <Link href="/" className="flex items-center">
-            <RiMoneyDollarCircleFill size={35} />
-            <h1 className="text-xl font-bold ml-1">FINEances</h1>
-          </Link>
-        </div>
-        <div
-          className="relative h-8 w-8 top-0 right-3 cursor-pointer"
-          onClick={toggleProfileMenu}
-        >
-          <ProfileImage />
-        </div>
+      <>
+        <header className="fixed flex justify-between items-center w-full px-3 h-12 bg-white border-b border-black/30">
+          <div className="flex justify-start items-center h-full w-1/2">
+            <Link href="/" className="flex items-center gap-x-2">
+              <RiMoneyDollarCircleFill size={35} />
+              <h1 className="text-xl font-bold">FINEances</h1>
+            </Link>
+          </div>
+          <ProfileImage onClick={toggleProfileMenu} />
+        </header>
         {showProfileMenu && (
           <div
             className="fixed w-full h-[200vh] bg-black/10"
             onClick={toggleProfileMenu}
           ></div>
         )}
-        <ProfileMenu isVisible={showProfileMenu} onClose={toggleProfileMenu} />
-      </header>
+        <Menu isVisible={showProfileMenu} onClose={toggleProfileMenu} />
+      </>
     );
   }
   return <div></div>;
