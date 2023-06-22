0be2e8f0adbbe66f36b2eb1d544c4388414533c4
Michal <michalbystryk14@wp.pl>
2023-06-22 19:19:23 +0200

    create MenuItem component

---
+++ app/components/MenuItem.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import { IconType } from "react-icons/lib";
+
+interface ItemProps {
+  href: string;
+  icon: IconType;
+  text: string;
+  onClick?: () => void;
+}
+
+const MenuItem: React.FC<ItemProps> = ({ href, icon, text, onClick }) => {
+  const Icon = icon;
+
+  return (
+    <Link href={href} replace>
+      <li
+        className="flex items-center gap-x-2 p-2 rounded-md cursor-pointer hover:bg-black/5"
+        onClick={onClick}
+      >
+        <Icon size={20} />
+        {text}
+      </li>
+    </Link>
+  );
+};
+
+export default MenuItem;
