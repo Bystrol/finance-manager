aa0d6d60f2971eaec6c87e6612c22db31ebd04f8
Michał <michalbystryk14@wp.pl>
2023-07-03 17:59:16 +0200

    add email validation and updating function

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -2,19 +2,57 @@ import prisma from "../../lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  const validateEmail = (email: string) => {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+  };
+
   try {
-    const { email, image } = await req.json();
+    const { email, image, newEmail } = await req.json();
 
-    await prisma.user.update({
-      where: {
-        email,
-      },
-      data: {
-        image,
-      },
-    });
+    if (image || image === "") {
+      await prisma.user.update({
+        where: {
+          email,
+        },
+        data: {
+          image,
+        },
+      });
+    }
 
-    return new NextResponse("OK", { status: 200 });
+    if (newEmail === "") {
+      return new NextResponse("Invalid email", { status: 422 });
+    }
+
+    if (newEmail) {
+      if (!validateEmail(newEmail)) {
+        return new NextResponse("Invalid email", { status: 422 });
+      }
+
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+
+      if (user && newEmail !== user.email) {
+        await prisma.user.update({
+          where: {
+            email,
+          },
+          data: {
+            email: newEmail,
+          },
+        });
+      } else {
+        return new NextResponse(
+          "New email must be different from the current one",
+          { status: 422 }
+        );
+      }
+    }
+
+    return new NextResponse("Profile updated", { status: 200 });
   } catch {
     return new NextResponse("Bad request", { status: 400 });
   }
