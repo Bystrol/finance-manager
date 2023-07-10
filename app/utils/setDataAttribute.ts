13910af5e6cf83a19623ff0fa411d1bc813c954d
Michał <michalbystryk14@wp.pl>
2023-07-10 18:24:26 +0200

    create setDataAttribute function

---
+++ app/utils/setDataAttribute.ts
@@ -0,0 +1,7 @@
+export const setDataAttribute = (e: React.ChangeEvent<HTMLInputElement>) => {
+  if (e.target.value) {
+    e.target.setAttribute("data-input-active", "");
+  } else {
+    e.target.removeAttribute("data-input-active");
+  }
+};
