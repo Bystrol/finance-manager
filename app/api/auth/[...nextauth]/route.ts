c9294f77386602d49302d55daeba3411208445e1
Michał <michalbystryk14@wp.pl>
2023-07-10 18:20:35 +0200

    add name token

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -76,6 +76,9 @@ export const authOptions: AuthOptions = {
         if (session.email) {
           token.email = session.email;
         }
+        if (session.name) {
+          token.name = session.name;
+        }
       }
       return token;
     },
