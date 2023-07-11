54851cf7d1301f7aeb81e0a3f0b341c8810f40e4
Michał <michalbystryk14@wp.pl>
2023-07-11 19:23:24 +0200

    remove status checking

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,8 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { Triangle } from "react-loader-spinner";
-
 const Home = () => {
-  const { status } = useSession();
-
-  if (status === "unauthenticated") {
-    redirect("/auth");
-  }
-
-  if (status === "authenticated") {
-    return (
-      <main className="min-h-screen flex flex-col items-center justify-center text-4xl font-bold"></main>
-    );
-  }
-
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center text-4xl font-bold">
-      <Triangle width={100} height={100} wrapperClass="absolute" color="#fff" />
-    </main>
+    <main className="min-h-screen flex flex-col items-center justify-center text-4xl font-bold"></main>
   );
 };
 
