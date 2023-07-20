6a4e21f0819c57ccbeed934b1e3dc6a592ee80ae
Michał <michalbystryk14@wp.pl>
2023-07-21 00:12:30 +0200

    add margin on top

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,10 @@
-import Header from "@/components/Header";
+import Header from '@/components/Header';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
       <Header />
-      <main className="flex justify-center items-center">{children}</main>
+      <main className="flex justify-center items-center mt-4">{children}</main>
     </div>
   );
 };
