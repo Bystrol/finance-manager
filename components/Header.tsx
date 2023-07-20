a03de56a78946276277c3971b451f88fe04bc9df
Michal <michalbystryk14@wp.pl>
2023-07-20 16:01:38 +0200

    remove useCallback, change, change to client comp

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,6 @@
-import { useCallback, useState } from "react";
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import ProfileImage from "./ProfileImage";
 import Menu from "./Menu/Menu";
@@ -7,13 +9,13 @@ import { RiMoneyDollarCircleFill } from "react-icons/ri";
 const Header: React.FC = () => {
   const [showProfileMenu, setShowProfileMenu] = useState<boolean>(false);
 
-  const toggleProfileMenu = useCallback(() => {
+  const toggleProfileMenu = () => {
     setShowProfileMenu((state) => !state);
-  }, []);
+  };
 
   return (
-    <>
-      <header className="fixed top-0 left-0 flex justify-between items-center w-full px-3 h-12 bg-white border-b border-black/30 z-10">
+    <header>
+      <div className="flex justify-between items-center w-full px-3 h-12 bg-white border-b border-black/30">
         <div className="flex justify-start items-center h-full w-1/2">
           <Link href="/" className="flex items-center gap-x-2">
             <RiMoneyDollarCircleFill size={35} />
@@ -21,15 +23,15 @@ const Header: React.FC = () => {
           </Link>
         </div>
         <ProfileImage onClick={toggleProfileMenu} />
-      </header>
+      </div>
       {showProfileMenu && (
         <div
           className="fixed top-0 left-0 w-full h-[200vh] bg-black/10 z-10"
           onClick={toggleProfileMenu}
-        ></div>
+        />
       )}
-      <Menu isVisible={showProfileMenu} onClose={toggleProfileMenu} />
-    </>
+      <Menu isVisible={showProfileMenu} closeMenu={toggleProfileMenu} />
+    </header>
   );
 };
 
