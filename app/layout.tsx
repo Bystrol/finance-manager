2e1cc18a9361a32807343f822e8eb5179b5629ba
Michał <michalbystryk14@wp.pl>
2023-07-22 19:30:11 +0200

    add redux store provider and loading component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css';
 import NextSessionProvider from '@/providers/NextSessionProvider';
 import ToasterProvider from '@/providers/ToasterProvider';
 import { AuthProvider } from '@/providers/AuthProvider';
+import { Provider as StoreProvider } from 'react-redux';
+import { store } from '@/store/store';
+import Loading from '@/features/loading/Loading';
 
 export const metadata = {
   title: 'FINEances',
@@ -18,12 +21,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        <NextSessionProvider>
-          <AuthProvider>
-            <ToasterProvider />
-            {children}
-          </AuthProvider>
-        </NextSessionProvider>
+        <StoreProvider store={store}>
+          <NextSessionProvider>
+            <AuthProvider>
+              <ToasterProvider />
+              {children}
+              <Loading />
+            </AuthProvider>
+          </NextSessionProvider>
+        </StoreProvider>
       </body>
     </html>
   );
