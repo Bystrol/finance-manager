38318ce835f0644883addfc8e1879866554f2675
Michal <michalbystryk14@wp.pl>
2023-07-25 19:17:03 +0200

    feat: create date.ts; add date constants

---
+++ constants/date.ts
@@ -0,0 +1,23 @@
+const currentDate: Date = new Date();
+const currentMonthNumber: number = currentDate.getMonth();
+export const monthsArray: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+export const currentMonthName: string = monthsArray[currentMonthNumber];
+export const currentYear: number = currentDate.getFullYear();
+export const yearsArray: number[] = [];
+
+for (let i = currentYear; i >= currentYear - 10; i--) {
+  yearsArray.push(i);
+}
