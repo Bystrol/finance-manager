e9751909e264fddfbc5818217ca9919e29d314bb
Michal <michalbystryk14@wp.pl>
2023-07-26 12:56:48 +0200

    feat: add apply button

--- components/UI/ModalCart.tsx
+++ components/UI/ModalCart.tsx
@@ -5,15 +5,21 @@ interface ModalCartProps {
 
 const ModalCart: React.FC<ModalCartProps> = ({ children, onClick }) => {
   return (
-    <>
+    <div className="fixed top-0 left-0 flex justify-center items-center w-full h-screen">
       <div
-        className="fixed top-0 left-0 flex justify-center items-center w-full h-screen bg-black/50"
+        className="fixed top-0 left-0 w-full h-screen bg-black/50"
         onClick={onClick}
       />
       <div className="flex flex-col justify-center items-center w-3/4 py-6 gap-4 bg-white rounded-md shadow-md z-10">
         {children}
+        <button
+          className="w-1/2 h-10 bg-black text-white font-bold rounded-md hover:bg-zinc-700"
+          onClick={onClick}
+        >
+          Apply
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
