5e3c164cdc570567efa6ad017a6f888d5ac1fffc
Michał <michalbystryk14@wp.pl>
2023-07-26 22:04:29 +0200

    feat: replace dummy list with real data

--- components/Balance/TransactionsList.tsx
+++ components/Balance/TransactionsList.tsx
@@ -1,48 +1,22 @@
 import Transaction from '@/components/Balance/Transaction';
-import { TransactionData } from '@/interfaces/operation_interfaces';
-import { currentDay, currentMonthName, currentYear } from '@/constants/date';
-import { IoFastFoodOutline } from 'react-icons/io5';
-import { GiClothes } from 'react-icons/gi';
-import { FaBus } from 'react-icons/fa';
-
-const transactionsList = [
-  {
-    description: 'description',
-    category: 'category',
-    amount: 100,
-    type: 'income',
-    date: String(currentMonthName + ' ' + currentDay + ', ' + currentYear),
-    icon: IoFastFoodOutline,
-  },
-  {
-    description: 'description2',
-    category: 'category2',
-    amount: 200,
-    type: 'income',
-    date: String(currentMonthName + ' ' + currentDay + ', ' + currentYear),
-    icon: GiClothes,
-  },
-  {
-    description: 'description3',
-    category: 'category3',
-    amount: 300,
-    type: 'expense',
-    date: String(currentMonthName + ' ' + currentDay + ', ' + currentYear),
-    icon: FaBus,
-  },
-];
+import { useSelector } from 'react-redux';
+import { RootState } from '@/store/store';
 
 const TransactionsList: React.FC = () => {
+  const transactions = useSelector(
+    (state: RootState) => state.balance.transactions,
+  );
+
   return (
     <section className="flex flex-col w-full items-start mt-4 gap-2">
       <h2 className="font-medium">Transaction History</h2>
+      <hr className="w-full h-[2px] bg-zinc-100" />
       <div className="flex flex-col w-full gap-4">
-        {transactionsList.map((transaction) => {
+        {transactions.map((transaction) => {
           return (
             <Transaction
               key={transaction.description}
               description={transaction.description}
-              category={transaction.category}
               amount={transaction.amount}
               type={transaction.type}
               date={transaction.date}
@@ -50,6 +24,9 @@ const TransactionsList: React.FC = () => {
             />
           );
         })}
+        {transactions.length === 0 && (
+          <p className="text-sm mt-2">No transactions available.</p>
+        )}
       </div>
     </section>
   );
