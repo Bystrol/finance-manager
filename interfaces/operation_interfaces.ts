4b6b8afea05df41458ad4493c813d77a11b5a39c
Michal <michalbystryk14@wp.pl>
2023-07-26 15:46:55 +0200

    feat: add TransactionData interface

--- interfaces/operation_interfaces.ts
+++ interfaces/operation_interfaces.ts
@@ -1,4 +1,15 @@
+import { IconType } from 'react-icons';
+
 export interface FilterProps {
   month: string;
   year: number;
+}
+
+export interface TransactionData {
+  description: string;
+  category: string;
+  amount: number;
+  type: string;
+  date: string;
+  icon: IconType | undefined | string;
 }
