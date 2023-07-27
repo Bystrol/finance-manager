60bb85a7489a459961b979ac3862609b4acb6637
Michal <michalbystryk14@wp.pl>
2023-07-27 09:09:09 +0200

    refactor: remove unused hook

--- components/Balance/IncomeModal.tsx
+++ components/Balance/IncomeModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { TransactionData } from '@/interfaces/operation_interfaces';
 import ModalCart from '@/components/UI/ModalCart';
