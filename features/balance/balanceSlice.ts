518c9fa616ba841c11554236a807c7bb59bb46f8
Michal <michalbystryk14@wp.pl>
2023-07-27 14:15:44 +0200

    bugfix: add missing data; change data name

--- features/balance/balanceSlice.ts
+++ features/balance/balanceSlice.ts
@@ -1,20 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { TransactionData } from '@/interfaces/operation_interfaces';
+import { currentMonthName, currentYear } from '@/constants/date';
 
-interface BalanceData {
+interface BalanceSliceData {
+  month: string;
+  year: number;
   totalAmount: number;
   transactions: TransactionData[];
 }
 
-const initialBalanceData: BalanceData = {
+const initialBalanceSliceData: BalanceSliceData = {
+  month: currentMonthName,
+  year: currentYear,
   totalAmount: 0,
   transactions: [],
 };
 
 const balanceSlice = createSlice({
   name: 'balance',
-  initialState: initialBalanceData,
+  initialState: initialBalanceSliceData,
   reducers: {
     addIncome(state, action: PayloadAction<TransactionData>) {
       state.transactions = [...state.transactions, action.payload];
