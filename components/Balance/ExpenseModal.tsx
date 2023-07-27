ddbeff389dc97b9c87cada9e55697796fa02b7c8
Michal <michalbystryk14@wp.pl>
2023-07-27 09:04:16 +0200

    feat: create ExpenseModal.tsx

---
+++ components/Balance/ExpenseModal.tsx
@@ -0,0 +1,123 @@
+'use client';
+
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { TransactionData } from '@/interfaces/operation_interfaces';
+import ModalCart from '@/components/UI/ModalCart';
+import { currentDay, currentMonthName, currentYear } from '@/constants/date';
+import { IoFastFoodOutline } from 'react-icons/io5';
+import { GiClothes } from 'react-icons/gi';
+import { FaBusAlt } from 'react-icons/fa';
+import { BsQuestionSquare } from 'react-icons/bs';
+import { addExpense } from '@/features/balance/balanceSlice';
+
+interface ExpenseModalProps {
+  onClose: () => void;
+}
+
+const initialTransactionData = {
+  description: '',
+  category: '',
+  amount: 0,
+  type: 'expense',
+  date: String(currentMonthName + ' ' + currentDay + ', ' + currentYear),
+  icon: BsQuestionSquare,
+};
+
+const ExpenseModal: React.FC<ExpenseModalProps> = ({ onClose }) => {
+  const [transactionData, setTransactionData] = useState<TransactionData>(
+    initialTransactionData,
+  );
+
+  const dispatch = useDispatch();
+
+  const setCategory = (category: string) => {
+    let icon;
+
+    switch (category) {
+      case 'Food':
+        icon = IoFastFoodOutline;
+        break;
+      case 'Clothes':
+        icon = GiClothes;
+        break;
+      case 'Transportation':
+        icon = FaBusAlt;
+      default:
+        icon = BsQuestionSquare;
+    }
+
+    setTransactionData({
+      ...transactionData,
+      category,
+      icon: icon,
+    });
+  };
+
+  return (
+    <ModalCart onClick={onClose}>
+      <div className="flex w-4/5 justify-between items-center">
+        <label htmlFor="description" className="font-bold">
+          Description
+        </label>
+        <input
+          id="description"
+          type="text"
+          className="w-3/5 h-10 border border-zinc-300 rounded-md px-2"
+          onChange={(e) => {
+            setTransactionData({
+              ...transactionData,
+              description: e.target.value,
+            });
+          }}
+        />
+      </div>
+      <div className="flex w-4/5 justify-between items-center">
+        <label htmlFor="category" className="font-bold">
+          Category
+        </label>
+        <select
+          name="category"
+          id="category"
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+          className="w-3/5 h-10 border border-zinc-300 rounded-md px-2"
+          defaultValue="- select -"
+        >
+          <option disabled>- select -</option>
+          <option>Food</option>
+          <option>Clothes</option>
+          <option>Transportation</option>
+        </select>
+      </div>
+      <div className="flex w-4/5 justify-between items-center">
+        <label htmlFor="amount" className="font-bold">
+          Amount
+        </label>
+        <input
+          id="amount"
+          type="number"
+          className="w-3/5 h-10 border border-zinc-300 rounded-md px-2"
+          onChange={(e) => {
+            setTransactionData({
+              ...transactionData,
+              amount: Number(parseFloat(e.target.value).toFixed(2)),
+            });
+          }}
+        />
+      </div>
+      <button
+        className="w-1/2 h-10 bg-black text-white font-bold rounded-md hover:bg-zinc-700 mt-4"
+        onClick={() => {
+          dispatch(addExpense(transactionData));
+          onClose();
+        }}
+      >
+        Add expense
+      </button>
+    </ModalCart>
+  );
+};
+
+export default ExpenseModal;
