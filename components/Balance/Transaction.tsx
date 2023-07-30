2088d7931418a349d4ec2bef07e71a85435c537c
Michał <michalbystryk14@wp.pl>
2023-07-30 19:17:54 +0200

    feat: add category prop; add EditModal

--- components/Balance/Transaction.tsx
+++ components/Balance/Transaction.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import EditModal from '@/components/Balance/EditModal';
 import { deleteTransaction } from '@/features/balance/balanceSlice';
 import { IconType } from 'react-icons';
 import { FiEdit } from 'react-icons/fi';
@@ -11,6 +12,7 @@ interface TransactionProps {
   amount: number;
   type: string;
   dateText: string;
+  category: string;
   icon: IconType;
 }
 
@@ -20,54 +22,80 @@ const Transaction: React.FC<TransactionProps> = ({
   amount,
   type,
   dateText,
+  category,
   icon: Icon,
 }) => {
   const [showOptionsCart, setShowOptionsCart] = useState<boolean>(false);
+  const [showEditModal, setShowEditModal] = useState<boolean>(false);
 
   const dispatch = useDispatch();
 
   const toggleOptionsCart = () => {
-    setShowOptionsCart((prevShowOptionsCart) => !prevShowOptionsCart);
+    setShowOptionsCart((prevState) => !prevState);
+  };
+
+  const closeOptionsCart = () => {
+    setShowOptionsCart(false);
+  };
+
+  const toggleModal = () => {
+    setShowEditModal((prevState) => !prevState);
   };
 
   return (
-    <div
-      tabIndex={1}
-      className="relative flex justify-between items-center bg-white rounded-lg p-3 shadow-md cursor-pointer"
-      onClick={toggleOptionsCart}
-      onBlur={toggleOptionsCart}
-    >
-      <div className="flex items-center gap-3">
-        <Icon size={30} />
-        <div className="flex flex-col items-start">
-          <p className="text-md">{description}</p>
-          <p className="text-xs">{dateText}</p>
-        </div>
-      </div>
-      <p
-        className={`text-lg font-bold ${
-          type === 'Incomes' ? 'text-emerald-500' : 'text-red-700'
-        }`}
+    <>
+      <div
+        tabIndex={1}
+        className="relative flex justify-between items-center bg-white rounded-lg p-3 shadow-md cursor-pointer"
+        onClick={toggleOptionsCart}
+        onBlur={closeOptionsCart}
       >
-        {type === 'Incomes' ? '+' : '-'}
-        {amount} PLN
-      </p>
-      {showOptionsCart && (
-        <div className="flex justify-center items-center gap-4 absolute top-0 left-0 w-full h-full bg-white rounded-lg text-sm font-bold">
-          <div className="flex justify-center items-center gap-1 hover:scale-110 transition-all">
-            <FiEdit />
-            <p>EDIT</p>
-          </div>
-          <div
-            className="flex justify-center items-center gap-1 hover:scale-110 transition-all"
-            onClick={() => dispatch(deleteTransaction({ id }))}
-          >
-            <RiDeleteBin6Line />
-            <p>DELETE</p>
+        <div className="flex items-center gap-3">
+          <Icon size={30} />
+          <div className="flex flex-col items-start">
+            <p className="text-md">{description}</p>
+            <p className="text-xs">{dateText}</p>
           </div>
         </div>
+        <p
+          className={`text-lg font-bold ${
+            type === 'Incomes' ? 'text-emerald-500' : 'text-red-700'
+          }`}
+        >
+          {type === 'Incomes' ? '+' : '-'}
+          {amount} PLN
+        </p>
+        {showOptionsCart && (
+          <div className="flex justify-center items-center gap-4 absolute top-0 left-0 w-full h-full bg-white rounded-lg text-sm font-bold">
+            <div
+              className="flex justify-center items-center gap-1 hover:scale-110 transition-all"
+              onClick={toggleModal}
+            >
+              <FiEdit />
+              <p className="cursor-pointer">EDIT</p>
+            </div>
+            <div
+              className="flex justify-center items-center gap-1 hover:scale-110 transition-all"
+              onClick={() => dispatch(deleteTransaction({ id }))}
+            >
+              <RiDeleteBin6Line />
+              <p className="cursor-pointer">DELETE</p>
+            </div>
+          </div>
+        )}
+      </div>
+      {showEditModal && (
+        <EditModal
+          id={id}
+          type={type}
+          icon={Icon}
+          description={description}
+          category={category}
+          amount={amount}
+          onClose={() => setShowEditModal(false)}
+        />
       )}
-    </div>
+    </>
   );
 };
 
