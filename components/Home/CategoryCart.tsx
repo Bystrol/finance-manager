84316d2566d13ca5905045e2e04380eca86becc1
Michal <michalbystryk14@wp.pl>
2023-08-07 18:36:05 +0200

    feat: create CategoryCart.tsx

---
+++ components/Home/CategoryCart.tsx
@@ -0,0 +1,24 @@
+import { CategoryCartData } from '@/interfaces/operation_interfaces';
+import { Progress } from 'flowbite-react';
+
+const CategoryCart: React.FC<CategoryCartData> = ({
+  icon: Icon,
+  category,
+  amount,
+  percentage,
+}) => {
+  return (
+    <div className="flex flex-col w-[47%] bg-white rounded-xl shadow-md p-4 gap-2">
+      <div className="flex justify-center items-center w-10 h-10 rounded-full bg-zinc-300">
+        <Icon size={20} />
+      </div>
+      <div className="flex flex-col gap-2">
+        <h2 className="text-md font-medium">{category}</h2>
+        <p className="text-sm">{amount} PLN</p>
+        <Progress progress={percentage} color="green" size="sm" />
+      </div>
+    </div>
+  );
+};
+
+export default CategoryCart;
