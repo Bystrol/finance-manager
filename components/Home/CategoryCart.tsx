997e9f49c40bd719fbf1b3dfed59513765df75b1
Michal <michalbystryk14@wp.pl>
2023-08-16 15:58:18 +0200

    feat: change width on large screens; add bar label

--- components/Home/CategoryCart.tsx
+++ components/Home/CategoryCart.tsx
@@ -8,14 +8,14 @@ const CategoryCart: React.FC<CategoryCartData> = ({
   percentage,
 }) => {
   return (
-    <div className="flex flex-col w-[47%] bg-white rounded-xl shadow-md p-4 gap-2">
+    <div className="flex flex-col w-[47%] lg:w-[30%] bg-white rounded-xl shadow-md p-4 gap-2">
       <div className="flex justify-center items-center w-10 h-10 rounded-full bg-zinc-300">
         <Icon size={20} />
       </div>
       <div className="flex flex-col gap-2">
         <h2 className="text-md font-medium">{category}</h2>
         <p className="text-sm">{amount} PLN</p>
-        <Progress progress={Math.round(percentage)} color="green" size="sm" labelProgress progressLabelPosition='outside'/>
+        <Progress progress={Math.round(percentage)} color="green" size="sm" labelProgress progressLabelPosition='outside' labelText textLabel='Expenses %' textLabelPosition='outside'/>
       </div>
     </div>
   );
